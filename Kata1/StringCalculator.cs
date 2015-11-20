7deb7cf59b0d9b92d00f09a41c94e91eb8964b96
ameneghini <alberto.meneghini@buy4now.com>
2015-11-20 17:32:26 +0000

    refactoring

--- Kata1/StringCalculator.cs
+++ Kata1/StringCalculator.cs
@@ -9,28 +9,42 @@ public class StringCalculator
     {
         if (string.IsNullOrEmpty(input))
             return 0;
+
+        input = UpdateDelimitersAndGetTheCleanInput(input);
+
+        int sum = 0;
+        CalculateNumbers(input, _delimiters, result =>
+         {
+             ProcessPossibleNegativeValues(result);
+             sum = result.Sum();
+         });
+
+        return sum;
+    }
+
+    private string UpdateDelimitersAndGetTheCleanInput(string input)
+    {
         if (input.Contains("\\"))
         {
             _delimiters += input[2];
             input = input.Substring(4);
         }
-        int sum = 0;
-        CalculateNumbers(input, result =>
-        {
-            var negatives = result.Where(integer => integer < 0);
-            if (negatives.Count() > 0)
-                throw new ArgumentOutOfRangeException(
-                    string.Format("Negatives not allowed {0}", string.Join(",", negatives.Select(x => x.ToString()).ToArray())
-                    ));
-            sum = result.Sum();
-        });
-        
-        return sum;
+        return input;
     }
 
-    private void CalculateNumbers(string input, Action<IEnumerable<int>> processResult)
+    private static void ProcessPossibleNegativeValues(IEnumerable<int> numbers)
     {
-        var numbers = input.Split(_delimiters.ToCharArray());
+        var negatives = numbers.Where(integer => integer < 0);
+        if (negatives.Count() > 0)
+            throw new ArgumentOutOfRangeException
+            (
+                string.Format("Negatives not allowed {0}", string.Join(",", negatives.Select(x => x.ToString()).ToArray()))
+             );
+    }
+
+    private static void CalculateNumbers(string input, string delimiters, Action<IEnumerable<int>> processResult)
+    {
+        var numbers = input.Split(delimiters.ToCharArray());
         if (numbers.Any(stringNumber => string.IsNullOrEmpty(stringNumber)))
             throw new ArgumentException();
         processResult(numbers.Select(number => int.Parse(number)));

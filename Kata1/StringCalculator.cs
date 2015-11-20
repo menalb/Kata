dc15bc145b93207bed730ccb7d9df15485bce3d3
ameneghini <alberto.meneghini@buy4now.com>
2015-11-20 17:04:07 +0000

    refactoring

--- Kata1/StringCalculator.cs
+++ Kata1/StringCalculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 public class StringCalculator
 {
@@ -13,16 +14,25 @@ public class StringCalculator
             _delimiters += input[2];
             input = input.Substring(4);
         }
+        int sum = 0;
+        CalculateNumbers(input, result =>
+        {
+            var negatives = result.Where(integer => integer < 0);
+            if (negatives.Count() > 0)
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Negatives not allowed {0}", string.Join(",", negatives.Select(x => x.ToString()).ToArray())
+                    ));
+            sum = result.Sum();
+        });
+        
+        return sum;
+    }
+
+    private void CalculateNumbers(string input, Action<IEnumerable<int>> processResult)
+    {
         var numbers = input.Split(_delimiters.ToCharArray());
         if (numbers.Any(stringNumber => string.IsNullOrEmpty(stringNumber)))
             throw new ArgumentException();
-        var integers = numbers.Select(number => int.Parse(number));
-                
-        var negatives = integers.Where(integer => integer < 0);
-        if (negatives.Count() > 0)
-            throw new ArgumentOutOfRangeException(
-                string.Format("Negatives not allowed {0}", string.Join(",", negatives.Select(x => x.ToString()).ToArray())
-                ));
-        return integers.Sum();
+        processResult(numbers.Select(number => int.Parse(number)));
     }
 }

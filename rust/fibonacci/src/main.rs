bd10dd4689733cb45aa1cf6fff32c62d47e9f517
Alberto Meneghini <menalb84@gmail.com>
2021-04-05 12:03:47 +0200

    fibonacci in rust attempt 1

---
+++ rust/fibonacci/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Hello, world!");
+    another_function(5);
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x)
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
